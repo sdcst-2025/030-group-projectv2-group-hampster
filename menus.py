9092b3ebd499458cd65753c78f556a27a4c89fdb
Sdsswt <terkeurswillem@gmail.com>
2025-01-28 11:27:33 -0800

    created choices

---
+++ menus.py
@@ -0,0 +1,45 @@
+# creating a menu
+#first get the input number for that calculator you want and add a way to prevent wrong numbers, then add all the calculators to the menu and make it so you can select them. Lastly make the menu look good by adding things to it.
+
+
+def title():
+        pass
+
+x=1
+while x==1:
+    title()
+
+    print("*list of calculators*")
+    choice=input('Enter a choice of calculator: ')
+    
+
+    if choice==0:
+          print("instructions: enter one of the numbers listed to relocate to that calculator, or to exit. The calculator will not work if you enter an invalid number...")
+    if choice==1:
+          
+    if choice==2:
+          
+    if choice==3:
+          
+    if choice==4:
+          
+    if choice==5:
+          
+    if choice==6:
+          
+    if choice==7:
+          
+    if choice==8:
+          
+    if choice==9:
+          
+    if choice==10:
+          
+    if choice==11:
+          
+    if choice==12:
+          
+    if choice==13:
+          
+    if choice==14:
+          exit()
