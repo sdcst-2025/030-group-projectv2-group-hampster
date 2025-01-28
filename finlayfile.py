7738c6f3dda31cf0241ccbe143df2d6cd11c5830
Finlay <finlay.mitchner@gmail.com>
2025-01-28 11:28:27 -0800

    Update finlayfile.py

---
+++ finlayfile.py
@@ -0,0 +1,13 @@
+def Acircle():
+    import math
+    import os
+    while val == false:
+        print("-------------------------\nPlease make a selection\n-------------------------")
+        choice = input('To solve for radius type "1" for radius \nTo solve for circumference type "2"\nTo solve for area type "3"\n')
+        os.system('cls')
+        if choice == 1 , 
+    if
+    r = input("enter radius of the circle")
+
+if __name__ == "__main__":
+    Acircle()
