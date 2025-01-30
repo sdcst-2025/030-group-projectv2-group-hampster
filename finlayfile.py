fee941c96b70bf7d1b9001445a63d8cc7f1145a5
Finlay <finlay.mitchner@gmail.com>
2025-01-30 11:09:05 -0800

    Finlay V.4

--- finlayfile.py
+++ finlayfile.py
@@ -36,7 +36,7 @@ def circle():
     while val == False:
         print("-------------------------\nPlease make a selection\n-------------------------")
         try:
-            choice = int(input('To solve for radius type "1" for radius \nTo solve for circumference type "2"\nTo solve for area type "3"\n'))
+            choice = int(input('To solve for radius type "1"\nTo solve for circumference type "2"\nTo solve for area type "3"\n'))
             time.sleep(1)
             os.system('cls')
             if choice in range(1,4):
@@ -68,8 +68,43 @@ def circle():
         return A
     
 
-
-
-
+def triangle():
+    import math
+    import os
+    import time
+    val = False
+    while val == False:
+        print("-------------------------\nPlease make a selection\n-------------------------")
+        try:
+            choice = int(input('To solve for height type "1" \n To solve for base type "2"\nTo solve for area type "3"\n'))
+            time.sleep(1)
+            os.system('cls')
+            if choice in range(1,4):
+                val = True
+            else: 
+                print("You entered an invalid input")
+        except:
+            print("You entered an invalid input")
+            time.sleep(1)
+            os.system('cls')
+            val = False
+    if choice == 1:
+        A = float(input("Please enter the area of the triangle: "))
+        b = float(input("Please enter the base of the triangle: "))
+        h = 2*A/b
+        print(f"The answer is {h}")
+        return h
+    elif choice == 2:
+        A = float(input("Please enter the area of the triangle: "))
+        h = float(input("Please enter the height of the triangle: "))
+        b = 2*A/h
+        print(f"The answer is {b}")
+        return b
+    elif choice == 3:
+        b = float(input("Please enter the base of the triangle: "))
+        h = float(input("Please enter the height of the triangle: "))
+        A = b*h/2
+        print(f"The answer is {A}")
+        return A
 if __name__ == "__main__":
-    circle()
+    triangle()
