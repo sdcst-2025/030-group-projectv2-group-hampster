2b5f721ad2300269f7e0ef6152ef9ea6f0632311
Sdsswt <terkeurswillem@gmail.com>
2025-02-03 10:26:51 -0800

    new update

--- menus.py
+++ menus.py
@@ -1,45 +1,88 @@
 # creating a menu
 #first get the input number for that calculator you want and add a way to prevent wrong numbers, then add all the calculators to the menu and make it so you can select them. Lastly make the menu look good by adding things to it.
+import os
+
+print("start calculating")
 
 
-def title():
-        pass
+print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+print("@@@@@@       @       @@      @@     @@%     @@@@@@@@@@:     @@@      @@:   %@@      @@*      @@@@@@@")
+print("@@@@@@@ @@@@ @@ @@@   @  @@@@@  @@@@@@  @@@@@@@@@@@@@@  @@@@@@@@@  @@@  @@@  @ @@@@  @@@@ .@@@@@@@@@")
+print("@@@@@@@      @@    @@@@  @@@@@@@@@@  @@@@@@  *@@@@@@@@@@@@@  %@@@  @@@       @     @@@@@@  @@@@@@@@@")
+print("@@@@@@  :@@@@@  @@    @       @     @@@     @@@@@@@@@@=     @@@@@  @@@  @@@  @  @@   @@@@  @@@@@@@@@")
+print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
 
-x=1
-while x==1:
-    title()
 
-    print("*list of calculators*")
-    choice=input('Enter a choice of calculator: ')
+
+input("")
+os.system('cls')
+
+
+
+
+
+
+import lucasfile
+import lucasfile as l
+
+
+
+print("*list of calculators*")
+print("0)instructions")
+print("1)rectangle calculator")
+print("2)calculator")
+print("3)calculator")
+print("2)calculator")
+print("3)calculator")
+print("2)calculator")
+print("3)calculator")
+print("2)calculator")
+print("3)calculator")
+print("2)calculator")
+print("3)calculator")
+print("2)calculator")
+print("3)calculator")
+
+choice=input('Enter a choice of calculator: ')
     
 
-    if choice==0:
-          print("instructions: enter one of the numbers listed to relocate to that calculator, or to exit. The calculator will not work if you enter an invalid number...")
-    if choice==1:
-          
-    if choice==2:
-          
-    if choice==3:
-          
-    if choice==4:
-          
-    if choice==5:
-          
-    if choice==6:
-          
-    if choice==7:
-          
-    if choice==8:
-          
-    if choice==9:
-          
-    if choice==10:
-          
-    if choice==11:
-          
-    if choice==12:
-          
-    if choice==13:
-          
-    if choice==14:
+if choice=='1':
+      print("instructions: enter one of the numbers listed to relocate to that calculator, or to exit.\n The calculator will not work if you enter an invalid number\n...")
+if choice=='2':
+      l.rectangle()
+'''if choice=='3':
+        volume of a sphere(lucas)
+if choice=='4':
+        area of a circle (finlay)
+if choice=='5':
+        
+if choice=='6':
+        
+if choice=='7':
+        
+if choice=='8':
+        
+if choice=='9':
+        
+if choice=='10':
+        
+if choice=='11':
+        
+if choice=='12':
+        
+if choice=='13':
+'''       
+if choice=='14':
           exit()
+
+
+#create a loan with compound interest and reacurring payment
+p = int(input("Enter the principal amount: "))
+r = int(input("Enter rate of interest: "))
+t = int(input("Enter time in years: " ))
+
+def compound_intrest(p,r,t):
+        Amount = p * (pow((1 + r / 100), t))
+        CI = Amount - p
+        print("Compound interest is", CI)
+
