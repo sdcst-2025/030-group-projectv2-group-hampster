b3252caefdd0d4892a0310f217fef572a60b50a7
Lucas Detlor <lucasjd0112@gmail.com>
2025-02-03 10:51:51 -0800

    v idk anymore

--- lucasfile.py
+++ lucasfile.py
@@ -49,7 +49,7 @@ def rectangle():
         s2 = float(input("Enter the second side length: "))
         ans = s1*s2
     os.system('cls')
-    ans = round(ans, 2)
+    ans = round(ans, R)
     print(f"The answer is {ans}.")
     return
 
@@ -82,7 +82,7 @@ def volsphere():
         rad = float(input("Enter the radius: "))
         ans = ((4/3)*math.pi*(rad**3))
     os.system('cls')
-    ans = round(ans, 2)
+    ans = round(ans, R)
     print(f"The answer is {ans}.")
     return
 
@@ -121,18 +121,49 @@ def rectangularprism():
         h = float(input("Enter the height: "))
         ans = l*w*h
     os.system('cls')
-    ans = round(ans, 2)
+    ans = round(ans, R)
     print(f"The answer is {ans}.")
     return
 
 def cylinder():
-    #DO ME NOW!
-    #IM NOT DONE YET
-    #DO IT
+    os.system('cls')
+    picked = 0
+    while picked == 0:
+        print('-------------------------\nPlease make a selection: \n-------------------------')
+        print('To solve for the radius, type "1"')
+        print('To solve for the surface area, type "2"')
+        print('To solve for the volume, type "3"')
+        try:
+            choice = int(input(""))
+            if choice > 0 and choice < 4:
+                picked = 1
+                os.system('cls')
+            else:
+                os.system('cls')
+                print('You didn\'t select an option.')
+        except:
+            os.system('cls')
+            print('You didn\'t select an option.')
+    if choice == 1:
+        h = float(input('Enter the height: '))
+        v = float(input('Enter the volume: '))
+        ans = (v/(math.pi*h))**(1/2)
+    if choice == 2:
+        r = float(input('Enter the radius: '))
+        h = float(input('Enter the height: '))
+        ans = (2*math.pi*r*h+2*math.pi*(r**2))
+    if choice == 3:
+        r = float(input('Enter the radius: '))
+        h = float(input('Enter the height: '))
+        ans = math.pi*(r**2)*h
+    os.system('cls')
+    ans = round(ans, R)
+    print(f"The answer is {ans}.")
+    return
 
 def simpleinterest():
     os.system('cls')
-    while picked = 0
+    while picked == 0:
         os.system('cls')
         print("-------------------------\nSimple Interest Calculator\n-------------------------")
         try:
@@ -144,7 +175,18 @@ def simpleinterest():
                 print("You didn't enter a valid input.")
         except:
             print("You didn't enter a valid input.")
-    
+    print('-------------------------\nSelect Compound Period\n-------------------------')
+    print('For every second, type " "')
+    print('For , type " "')
+    print('For , type " "')
+    print('For , type " "')
+    print('For , type " "')
+    print('For , type " "')
+    print('For , type " "')
+    print('For , type " "')
+    print('For , type " "')
+    print('For , type " "')
+    print('For , type " "')
     ans = round(ans, 2)
     print(f"The answer is {ans}.")
     return
@@ -152,7 +194,7 @@ def simpleinterest():
 
 
 if __name__ == "__main__":
-    simpleinterest()    
+    cylinder()
 
 
 '''
